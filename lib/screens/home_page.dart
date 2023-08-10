636d9ebe8a916879e301b8e62f7f1b162690bcb1
hsmulhim <79mulhim@gmail.com>
2023-08-10 15:06:50 +0300

    added floating button

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,7 +3,6 @@ import 'package:cards_app/main.dart';
 import 'package:cards_app/screens/wish_list.dart';
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -15,25 +14,28 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red.shade300,
-        title: Text('Products'),
-        leading: IconButton(onPressed: (){
-          Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => WishListScreen()));
-        }, icon: Icon(Icons.favorite_outline)),
-        actions: [IconButton(onPressed: (){}, icon: Icon(Icons.add))],
-      ),
-      body:SafeArea(child: 
-      ListView.builder(
-        itemBuilder: (context, int index) {
-          return CardWidget(product: ProductList![index]);
-        },
-        itemCount: ProductList!.length,
-      )
-      ,)
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.red.shade300,
+          title: Text('Products'),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WishListScreen()));
+              },
+              icon: Icon(Icons.favorite_outline)),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        ),
+        floatingActionButton: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.search),
+        ),
+        body: SafeArea(
+          child: ListView.builder(
+            itemBuilder: (context, int index) {
+              return CardWidget(product: ProductList![index]);
+            },
+            itemCount: ProductList!.length,
+          ),
+        ));
   }
 }
